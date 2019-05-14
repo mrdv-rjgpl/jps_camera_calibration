8c88119ca257644ba2b8bb5aee0580f6dd21c88e
Mardava Gubbi <mgubbi1@jhu.edu>
2019-05-14 09:09:10 -0400

    Update camera exposure setting
    This seems to change during the day, possibly depending on the camera
    temperature.

--- scripts/uvc_script.sh
+++ scripts/uvc_script.sh
@@ -1,4 +1,4 @@
 uvcdynctrl --device=/dev/video1 --set "Focus, Auto" 0
 uvcdynctrl --device=/dev/video1 --set "Focus (absolute)" 3
 uvcdynctrl --device=/dev/video1 --set "Exposure, Auto" 1
-uvcdynctrl --device=/dev/video1 --set "Exposure (absolute)" 50
+uvcdynctrl --device=/dev/video1 --set "Exposure (absolute)" 105
